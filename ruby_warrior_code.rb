a4a784ab6de1accdd84e1ec861ba44da0a011b95
migueog <migueog@gmail.com>
2013-09-13 23:05:04 -0400

    Update ruby_warrior_code.rb
    Added code until Level 6 of RubyWarrior
    Problems faced:
    -Was unable to have the warrior only move forward when the captive was rescued

--- ruby_warrior_code.rb
+++ ruby_warrior_code.rb
@@ -1,20 +1,26 @@
-class Player
+  class Player
   START_HEALING_AT_HEALTH_LEVEL = 15
-
+  rescued_prisoner = false
   def taking_damage?(current_health)
-    current_health < @health.to_i
-  end
+   current_health < @health.to_i 
+     end
 
   def play_turn(warrior)
-    if warrior.feel.enemy?
-      warrior.attack!
-    elsif !taking_damage?(warrior.health) && warrior.health < START_HEALING_AT_HEALTH_LEVEL
-      warrior.rest!
-    else
+    if warrior.feel(:backward).empty? && rescued_prisoner = false
+      warrior.walk!(:backward)
+    elsif warrior.feel(:backward).captive?
+      warrior.rescue!(:backward)
+      rescued_prisoner = true
+    elsif warrior.feel(:backward).wall?
       warrior.walk!
+    elsif warrior.feel(:forward).empty? && !rescued_prisoner = true
+      warrior.walk!
+    elsif warrior.feel.enemy?
+      warrior.attack!
+    else !taking_damage?(warrior.health) && warrior.health < START_HEALING_AT_HEALTH_LEVEL
+      warrior.rest!
     end
-    
+
     @health = warrior.health
   end
-end
-  
+end 
